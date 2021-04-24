0561f8cc54a1ac5c5c047e2513ec03e59a314950
Alejandro Alvarez <alejandro.alvarez@projectx.ai>
2021-04-24 23:17:40 +0200

    csv_gen.sh update path

--- csv_gen.sh
+++ csv_gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create train.csv
-find data/train -type f -regex ".*v[vh].png" -exec basename -s .png {} \; | sed -En -e'1i Place,Datetime,x,y,type' -e's@_@,@g' -e's@[xy]-@@gp' > train.csv
+find data/train -type f -regex ".*v[vh].png" -exec basename -s .png {} \; | sed -En -e'1i Place,Datetime,x,y,type' -e's@_@,@g' -e's@[xy]-@@gp' > data/train.csv
 
 # Create val.csv
-find data/val -type f -regex ".*v[vh].png" -exec basename -s .png {} \; | sed -En -e'1i Place,Datetime,x,y,type' -e's@_@,@g' -e's@[xy]-@@gp' > val.csv
+find data/val -type f -regex ".*v[vh].png" -exec basename -s .png {} \; | sed -En -e'1i Place,Datetime,x,y,type' -e's@_@,@g' -e's@[xy]-@@gp' > data/val.csv
